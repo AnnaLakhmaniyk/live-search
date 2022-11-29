20dfe4046a49c915eb2adec13bc58f79d4a2f581
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-11-29 15:21:42 +0200

    new scan

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let naborIN = new Set();
-console.log(naborIN);
-
 let si;
 
 let counter = -1;
+
+let naborIN = new Set();
+let arr = new Set();
 
 const scaner = async () => {
   if (counter === -1) {
@@ -18,15 +18,21 @@ const scaner = async () => {
   }
 
   const scanURI = [...naborIN][counter];
-  console.log(`сканується: ${scanURI}`);
+  // console.log(`сканується: ${scanURI}`);
 
   let str = await fetch(scanURI).then((resp) => resp.text());
 
-  let naborIN = new Set();
-
   let scanDoc = new DOMParser().parseFromString(str, "text/html");
   let arrHrefs = [...scanDoc.getElementsByTagName("a")].map((i) => i.href);
-  let aIN = arrHrefs.filter((i) => i.includes(location.origin));
+  let aIN = arrHrefs.filter((i) => {
+    if (i.includes("@")) {
+      const indexEl = i.indexOf(":") + 1;
+      arr.add(i.slice(indexEl));
+      return;
+    }
+
+    return i.includes(location.origin);
+  });
 
   aIN
     .map((i) => i.replace(/\?.+/g, "").replace(/\#.+/g, ""))
@@ -38,9 +44,38 @@ si = setInterval(scaner, 5000);
 
 //---------------------------------
 
-var src = document.getElementById("src").value;
-var parser = new DOMParser();
-var htmlDoc = parser.parseFromString(src, "text/html");
+// var src = document.getElementById("src").value;
+// var parser = new DOMParser();
+// var htmlDoc = parser.parseFromString(src, "text/html");
+
+///логіка отримання силки на сторінці
+
+// const scanURI = location.origin;
+// let arr = new Set();
+// let nabor = new Set();
+// console.log(arr);
+// console.log(nabor);
+
+// const parser = async () => {
+//   let str = await fetch(location.origin).then((resp) => resp.text());
+//   let scanDoc = new DOMParser().parseFromString(str, "text/html");
+
+//   [...scanDoc.getElementsByTagName("a")]
+//     .map((i) => i.href)
+//     .filter((i) => {
+//       if (i.includes("@")) {
+//         const indexEl = i.indexOf(":") + 1;
+//         arr.add(i.slice(indexEl));
+//         return;
+//       }
+
+//       return i.includes(location.origin);
+//     })
+//     .map((i) => i.replace(/\?.+/g, "").replace(/\#.+/g, ""))
+//     .map((i) => nabor.add(i));
+// };
+
+// parser();
 
 // window.addEventListener("DOMContentLoaded", () => {
 //   const body = document.querySelector("body");
